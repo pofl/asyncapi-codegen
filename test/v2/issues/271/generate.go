17fb9c83c927bb350e1fbcc5411b7b62c01b2800
Florian Polster <florian.polster@staffbase.com>
2025-03-07 17:47:45 +0100

    Add failing case

---
+++ test/v2/issues/271/generate.go
@@ -0,0 +1,5 @@
+//go:generate go run ../../../../cmd/asyncapi-codegen -p issue271 -i ./asyncapi.yaml -o ./asyncapi.gen.go
+
+package issue271
+
+// This is just to test that the generation is correct
